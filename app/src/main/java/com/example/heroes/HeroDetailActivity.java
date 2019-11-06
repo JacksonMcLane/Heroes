e5487048615bbe3ca948ce6ad05e484861982195
JacksonMcLane <jackson.mclane20@gmail.com>
2019-11-06 14:00:38 -0800

    Wired everything listView works incomplete intents

--- app/src/main/java/com/example/heroes/HeroDetailActivity.java
+++ app/src/main/java/com/example/heroes/HeroDetailActivity.java
@@ -2,13 +2,34 @@ package com.example.heroes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class HeroDetailActivity extends AppCompatActivity {
+
+    TextView textViewName;
+    TextView textViewDescription;
+    TextView textViewRanking;
+    TextView textViewSuperPower;
+    ImageView imageViewPicture;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero_detail);
+        wireWidgets();
+        Intent lastIntent = getIntent();
+        Hero hero = lastIntent.getParcelableExtra("Hero");
+
+
+    }
+
+    private void wireWidgets() {
+        textViewName = findViewById(R.id.textView_detail_name);
+        textViewDescription = findViewById(R.id.textView_detail_description_text);
+        textViewRanking = findViewById(R.id.textView_detail_rank_text);
+        textViewSuperPower = findViewById(R.id.textView_detail_power_text);
     }
 }
